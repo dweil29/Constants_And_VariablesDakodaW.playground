fb8d98d8f1b8a662aeb13bd92584049339fb2caf
dweil29 <dweil@students.mchenry.edu>
2017-01-27 10:03:02 -0600

    Constants_And_VariablesDakodaW
    Variable and Constant exercise

---
+++ Contents.swift
@@ -0,0 +1,35 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var number = 4444
+
+var decimal = 0.12
+
+var longDecimal = 7.27947262902
+
+
+let stayNumber = 12
+
+let stayDecimal = 3.14
+
+let stayLongDecimal = 3.14159265
+
+
+let newInt: Int = 827
+
+let newDeci: Float = 14.22
+
+let newLong: Double = 6.222244442222
+
+
+let testInt = 7
+testInt = 5
+
+
+let testFloat: Float = 4
+
+
+// Even though the constant has an explicit type, it detects that the value is an integer.
+
+
